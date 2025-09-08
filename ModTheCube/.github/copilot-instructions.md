4e7937bde60c7abb3f15dfb91dbc00bab05f8538
anujlunawat <anujjain0805@gmail.com>
2025-09-08 15:18:21 +0530

    ModTheCube

---
+++ ModTheCube/.github/copilot-instructions.md
@@ -0,0 +1,8 @@
+<!-- UNITY CODE ASSIST INSTRUCTIONS START -->
+- Project name: ModTheCube
+- Unity version: Unity 6000.2.2f1
+- Active game object:
+  - Name: Cube
+  - Tag: Untagged
+  - Layer: Default
+<!-- UNITY CODE ASSIST INSTRUCTIONS END -->
---
+++ ModTheCube/Assets/ModTheCube/Cube.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cube : MonoBehaviour
+{
+    public MeshRenderer Renderer;
+    private Color targetColor;
+
+    void Start()
+    {
+        float posX = Random.Range(0, 10);
+        float posY = Random.Range(0, 10);
+        float posZ = Random.Range(0, 10);
+        transform.position = new Vector3(posX, posY, posZ);
+        transform.localScale = Vector3.one * 1.3f;
+
+
+        //Material material = Renderer.material;
+        //material.color = new Color(0.5f, 1.0f, 0.3f, 1f);
+    }
+    
+    void Update()
+    {
+        Renderer.material.color = Color.Lerp(Renderer.material.color, targetColor, Time.deltaTime);
+
+        if(Vector4.Distance(Renderer.material.color, targetColor) < 0.05f)
+        {
+            SetNewTargetColor();
+        }
+
+        transform.Rotate(10.0f * Time.deltaTime, 0.0f, 0.0f);
+    }
+
+    void SetNewTargetColor()
+    {
+        targetColor = new Color(Random.value, Random.value, Random.value);
+    }
+}
